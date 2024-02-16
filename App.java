d1bf4b607b36752cf3cf5eb7c58917e339b62cf9
ailene808 <54044885+ailene808@users.noreply.github.com>
2024-02-15 19:10:24 -0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,21 +1,16 @@
 package mystudent;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("\n Hello from class App \n\n");
+    public static void main(String [] args){
 
-        // Create a student object
         Student myStudent = new Student();
+        myStudent.firstName = "Ailene";
+        myStudent.lastName = "Alexander";
+        myStudent.gpa = 4.0;
 
-        // Fill the new myStudent object's data fields
-        myStudent.firstName = "Dennis";
-        myStudent.lastname = "Mohle";
-        myStudent.gpa = 3.4;
-        myStudent.major = "Computer Science";
-        myStudent.city = "Fresno";
-
-        System.out.println("\n The first name of the student is: " + myStudent.firstName + "\n");
-        System.out.println("\n The last name of the student is: " + myStudent.lastname + "\n");
+        System.out.println(myStudent.firstName);
+        System.out.println(myStudent.lastName);
+        System.out.println(myStudent.gpa);
 
     }
 }
